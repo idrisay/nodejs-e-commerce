77cfe1257a4ca71ce6baaf0062374011a1bae4ea
Idris AY <97283155+idrisay@users.noreply.github.com>
2023-03-11 08:25:41 +0100

    feat: update products model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-  name: {
+  title: {
     type: String,
     required: true,
   },
   category: {
+    type: String,
+    required: true,
+  },
+  image_url: {
+    type: String,
+    required: true,
+  },
+  description: {
     type: String,
     required: true,
   },
